e67a0765b26caeabe33f9c10b00e2ac1a68b9eaf
Luciano Fraile <lucianofrailee@gmail.com>
2023-07-31 16:15:47 -0300

    database connected to the server

--- PI-Dogs-main/api/src/db.js
+++ PI-Dogs-main/api/src/db.js
@@ -30,10 +30,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+const { Dog, temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Dog.belongsToMany(temperament, {through: "dog_temperament"});
+temperament.belongsToMany(Dog, {through: "dog_temperament"});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
